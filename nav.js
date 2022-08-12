acae5a17d4f98e872f3d8b0bbc007e6f38238903
ddcroft73 <gen.disarray73@outlook.com>
2022-08-12 19:23:23 -0400

    design UI for basic node framework

---
+++ nav.js
@@ -0,0 +1,8 @@
+
+
+const openNav = () => {
+    document.getElementById("Sidenav").style.width = "180px";
+  }    
+const closeNav = () => {
+   document.getElementById("Sidenav").style.width = "0";
+  } 
