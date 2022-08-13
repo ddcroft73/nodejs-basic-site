d411902abacca7c02d656d9d7b04bbd979782408
ddcroft73 <gen.disarray73@outlook.com>
2022-08-12 20:02:17 -0400

    styled the pages a bit more.

--- nav.js
+++ nav.js
@@ -1,8 +1,11 @@
 
+const sideNav = document.querySelector('.sidenav');
 
 const openNav = () => {
     document.getElementById("Sidenav").style.width = "180px";
+    sideNav.style.boxShadow = '0px 0px 5px 5px rgba(0, 0, 0, 0.4), 0 0px 0px 0 rgba(27, 28, 27, 0.19)';
   }    
 const closeNav = () => {
    document.getElementById("Sidenav").style.width = "0";
+  sideNav.style.boxShadow = '0px 0px 0px 0px rgba(0, 0, 0, 0.4), 0 0px 0px 0 rgba(27, 28, 27, 0.19)';
   } 
