bef010ef31d89551627485025724445ee7f35706
ddcroft73 <gen.disarray73@outlook.com>
2022-08-13 19:43:24 -0400

    updated styles,sidenav mostly.

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ const sideNav = document.querySelector('.sidenav');
 
 const openNav = () => {
     document.getElementById("Sidenav").style.width = "180px";
-    sideNav.style.boxShadow = '0px 0px 5px 5px rgba(0, 0, 0, 0.4), 0 0px 0px 0 rgba(27, 28, 27, 0.19)';
+    sideNav.style.boxShadow = '0px 0px 5px 3px rgba(0, 0, 0, 0.2), 0 0px 0px 0 rgba(27, 28, 27, 0.19)';
   }    
 const closeNav = () => {
    document.getElementById("Sidenav").style.width = "0";
